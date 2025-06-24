bebb62be7c973bba90ca3b65af973a480731b70a
EddieRich <ehandrich@gmail.com>
2025-06-24 12:20:03 -0400

    basic-2-2 coded and cleaned, do not run this version

--- basic-2-2.c
+++ basic-2-2.c
@@ -1,7 +1,79 @@
-#include <X11.h>
-#include <Xutil.h>
+/* This program creates and displays a basic window.
+ * The window has a default white background.
+ *
+ * Coded by : Ross Maloney
+ * Date : August 2006
+ */
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
 int main(int argc, char **argv)
 {
+  Display *display;
+  XSetWindowAttributes winatt;
+  Window window;
+  XSizeHints wmsize;
+  XWMHints wmhints;
+  XTextProperty windowName, iconName;
+  XEvent event;
+  char *window_name = "Basic 2.2";
+  char *icon_name = "icon";
+  int screen_num, done;
+  unsigned long valuemask;
+
+  /* 1 .open connection to the server */
+
+  display = XOpenDisplay(NULL);
+
+  /* 2 .create a top-level window */
+
+  screen_num = DefaultScreen(display);
+  winatt.background_pixel = WhitePixel(display, screen_num);
+  winatt.border_pixel = BlackPixel(display, screen_num); // ignored by window manager
+  winatt.event_mask = ButtonPressMask;
+  valuemask = CWBackPixel | CWBorderPixel | CWEventMask;
+  window = XCreateWindow(display, RootWindow(display, screen_num),
+                         200, 200, 800, 600, 2,
+                         DefaultDepth(display, screen_num), InputOutput,
+                         DefaultVisual(display, screen_num),
+                         valuemask, &winatt);
+
+  /* 3 .give the Window Manager hints */
+
+  wmsize.flags = USPosition | USSize;
+  XSetWMNormalHints(display, window, &wmsize);
+  wmhints.initial_state = NormalState;
+  wmhints.flags = StateHint;
+  XSetWMHints(display, window, &wmhints);
+  XStringListToTextProperty(&window_name, 1, &windowName);
+  XSetWMName(display, window, &windowName);
+  XStringListToTextProperty(&icon_name, 1, &iconName);
+  XSetWMIconName(display, window, &iconName);
+
+  /* 7 .map the windows */
+
+  XMapWindow(display, window);
+
+  /* 8 .enter the event loop */
+
+  done = 0;
+  while (done == 0)
+  {
+    XNextEvent(display, &event);
+    switch (event.type)
+    {
+    case ButtonPress:
+      break;
+    }
+  }
+
+  /* 9 .cleanup before exiting */
+
+  XUnmapWindow(display, window);
+  XDestroyWindow(display, window);
+  XCloseDisplay(display);
+
+  /* you must return a value, author forgot this*/
   return 0;
 }
